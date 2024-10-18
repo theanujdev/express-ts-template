0a5e8a23bf02d562ad6f589db845256eef4fee46
Anuj Sharma <69591862+theanujdev@users.noreply.github.com>
2024-10-18 17:44:37 +0530

    feat: add eslint for ts and eslint-config-prettier plugin

---
+++ eslint.config.mjs
@@ -0,0 +1,53 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  // global ignores
+  {
+    ignores: ["dist/", "node_modules/"],
+  },
+
+  // applies to everything
+  eslint.configs.recommended,
+
+  // applies only to ts files
+  {
+    name: "tseslint",
+    files: ["src/**/*.ts"],
+    extends: [
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.strictTypeChecked,
+    ],
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true,
+        // projectService: true,
+        // tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "no-console": "warn",
+    },
+  },
+
+  // global variables, applies to everything
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+
+  // prettier config
+  prettier
+);
